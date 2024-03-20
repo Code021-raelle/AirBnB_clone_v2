fc191a4526fbb9fe1938f10aeef87c00cd25142a
Code021-raelle <gabrielakinshola3@gmail.com>
2024-03-20 13:05:50 +0100

    bnb

--- models/__init__.py
+++ models/__init__.py
@@ -3,7 +3,7 @@
 from os import getenv
 
 
-if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+if getenv('HBNB_TYPE_STORAGE') == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
