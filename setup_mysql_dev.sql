49e30ec5c2df994f91d58228af82fb622d508228
Code021-raelle <gabrielakinshola3@gmail.com>
2024-03-23 12:04:06 +0100

    bnbV2

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -9,6 +9,3 @@ GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 -- Grant SELECT privilege on performance_schema to hbnb_dev
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-
--- Flush privileges to ensure the changes take effect
-FLUSH PRIVILEGES;
