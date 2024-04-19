3c92befe848387612bbf9dd12320d3a7436aa0a3
Code021-raelle <gabrielakinshola3@gmail.com>
2024-04-19 08:17:37 -0700

    web_flask

--- models/state.py
+++ models/state.py
@@ -3,11 +3,13 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
+import os
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
+    __table_args__ = {'extend_existing': True}
     name = Column(String(128), nullable=False)
     cities = relationship(
             "City", backref="state", cascade="all, delete")
